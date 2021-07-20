e8ce753a7e4a2c0e78dba0e4db5343e83cc455f3
imchanyang <cksdid636@naver.com>
2021-07-21 01:43:10 +0900

    Numpy_Study2

--- Numpy4.py
+++ Numpy4.py
@@ -8,15 +8,16 @@ array.sort()
 print(array)
 print(array[::-1])
 
-# 각 열을 기준으로 정렬
+# 각 행을 기준으로 정렬
 array_ = np.array([[5, 9, 10, 3, 1], [8, 3, 4, 2, 4]])
-print(array)
+print(array_)
 array_.sort(axis=0)
-print(array)
+print(array_)
 
 # 균일한 간격으로 데이터 생성(0부터 10사이를 5개 데이터로 채운다)
 array__ = np.linspace(0, 10, 5)
-print(array_)
+print(array__)
+
 
 # 난수의 재연 (실행마다 결과 동일)
 np.random.seed(7)
@@ -30,10 +31,13 @@ array_2[0] = 777
 print(array_1)
 
 # 행렬 자체가 array_2에 들어간다. 이것을 방지하기위해 np.unique()를 사용한다.
+array_1 = np.arange(0, 10)
 array_3 = array_1.copy()
 array_3[0] = 999
+print(array_1)
 print(array_1)
 
 # 중복된 원소 제거
 array___ = np.array([1, 1, 2, 2, 2, 3, 3, 4])
+print(array___)
 print(np.unique(array___))
